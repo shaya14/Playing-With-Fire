5dab2e3b864aada3eae42e1f121f8cfd7e6e5774
Shay yosef <shayagres@gmail.com>
2023-12-19 15:53:45 +0200

    Add custom option to player movement

--- Playing With Fire/Assets/Scripts/PlayerController.cs
+++ Playing With Fire/Assets/Scripts/PlayerController.cs
@@ -1,19 +1,26 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
-enum PlayerInput
+public enum PlayerInput
 {
     WASD,
-    ArrowKeys
+    ArrowKeys,
+    Custom
 }
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] PlayerInput _playerInput;
+    public PlayerInput _playerInput;
     [SerializeField] string _playerName;
     [SerializeField] float _moveSpeed;
     [SerializeField] Bomb _bombPrefab;
     Rigidbody2D _rb;
+    [HideInInspector] public KeyCode _upKey;
+    [HideInInspector]  public KeyCode _downKey;
+    [HideInInspector]  public KeyCode _leftKey;
+    [HideInInspector]  public KeyCode _rightKey;
+    [HideInInspector]  public KeyCode _bombKey;
 
     void Awake()
     {
@@ -95,9 +102,39 @@ public class PlayerController : MonoBehaviour
                     _rb.MovePosition(movement);
                 }
                 break;
+            case PlayerInput.Custom:
+                if (Input.GetKey(_upKey))
+                {
+                    //Move Up
+                    Vector3 direction = Vector3.up * _moveSpeed * Time.deltaTime;
+                    Vector3 movement = transform.position + direction;
+                    _rb.MovePosition(movement);
+                }
+                else if (Input.GetKey(_leftKey))
+                {
+                    //Move Left
+                    Vector3 direction = Vector3.left * _moveSpeed * Time.deltaTime;
+                    Vector3 movement = transform.position + direction;
+                    _rb.MovePosition(movement);
+                }
+                else if (Input.GetKey(_downKey))
+                {
+                    //Move Down
+                    Vector3 direction = Vector3.down * _moveSpeed * Time.deltaTime;
+                    Vector3 movement = transform.position + direction;
+                    _rb.MovePosition(movement);
+                }
+                else if (Input.GetKey(_rightKey))
+                {
+                    //Move Right
+                    Vector3 direction = Vector3.right * _moveSpeed * Time.deltaTime;
+                    Vector3 movement = transform.position + direction;
+                    _rb.MovePosition(movement);
+                }
+                break;
             default:
                 break;
-        }     
+        }
     }
 
     void SpawnBomb()
@@ -118,9 +155,44 @@ public class PlayerController : MonoBehaviour
                     var bomb = Instantiate(_bombPrefab, transform.position, Quaternion.identity);
                 }
                 break;
+            case PlayerInput.Custom:
+                if (Input.GetKeyDown(_bombKey))
+                {
+                    //Spawn bomb
+                    var bomb = Instantiate(_bombPrefab, transform.position, Quaternion.identity);
+                }
+                break;
             default:
                 break;
         }
         //Spawn bomb
+    }
+}
+
+[CustomEditor(typeof(PlayerController))]
+public class PlayerControllerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+        PlayerController playerController = (PlayerController)target;
+
+        if (playerController._playerInput == PlayerInput.Custom)
+        {
+            EditorGUILayout.Space(); // Add some space to separate custom input fields from the rest
+
+            EditorGUI.BeginChangeCheck();
+
+            playerController._upKey = (KeyCode)EditorGUILayout.EnumPopup("Up Key", playerController._upKey);
+            playerController._downKey = (KeyCode)EditorGUILayout.EnumPopup("Down Key", playerController._downKey);
+            playerController._leftKey = (KeyCode)EditorGUILayout.EnumPopup("Left Key", playerController._leftKey);
+            playerController._rightKey = (KeyCode)EditorGUILayout.EnumPopup("Right Key", playerController._rightKey);
+            playerController._bombKey = (KeyCode)EditorGUILayout.EnumPopup("Bomb Key", playerController._bombKey);
+
+            if (EditorGUI.EndChangeCheck())
+            {
+                EditorUtility.SetDirty(playerController);
+            }
+        }
     }
 }
